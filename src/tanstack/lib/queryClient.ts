704e132003346444df0272cfcb35f3c279169957
dickydev <dickyfernando062@gmail.com>
2026-01-09 20:13:04 +0700

    feat: tambah json-server

--- src/tanstack/lib/queryClient.ts
+++ src/tanstack/lib/queryClient.ts
@@ -1,3 +1,12 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient({});
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 10_000, // staleTime -> berapa lama data dianggap MASIH FRESH (masih dipercaya)
+      gcTime: 5 * 60_000, // cacheTime / gcTime (gc = garbage collection) -> berapa lama data cache disimpan di memori
+      refetchOnWindowFocus: true,
+      retry: 1,
+    },
+  },
+});
